eae65037f34267f37f0752fcf06012c70340a22a
Mohd Faizan <42490239+Mohdfaizan-3@users.noreply.github.com>
2023-02-13 22:49:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,25 @@
 //your code here
 
+
+const btn = document.querySelector('#btn');
+const display = document.querySelector('#num');
+const UserInput = Number(document.querySelector('#guess').value);
+const response = document.getElementById("respond");
+
+function run(e) {
+	const res = Math.floor(Math.random()*40) -20;
+	display.innerText=res;
+	guess(res);
+}
+
+function guess(res) {
+	if(Math.abs(res - UserInput) <= 10){
+		response.innerText = 'Hot';
+	}else{
+		response.innerText = 'Cold';
+	}
+}
+
+btn.addEventListener('click',run);
+
+
